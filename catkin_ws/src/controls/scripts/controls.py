403d36494d8c4dd225fd190c10901280eadd1037
Russell Buchanan <russellaabuchanan@gmail.com>
2014-11-02 16:09:39 -0500

    Add SetPostition and SetVelovity subscribers

--- catkin_ws/src/controls/scripts/controls.py
+++ catkin_ws/src/controls/scripts/controls.py
@@ -1,24 +1,60 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from auv_msgs.msg import SetPosition
+from auv_msgs.msg import SetVelocity
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
+xPos = 0.0
+yPos = 0.0
+
+surgeSpeed = 0.0
+swaySpeed = 0.0
+
+depth = 0.0
+roll = 0.0
+pitch = 0.0
+yaw = 0.0
+
+isSettingPosition = 0
+
+def setPosition_callback(data):
+    rospy.loginfo(rospy.get_caller_id()+"I heard setPosition xPos = %f, yPos = %f, depth = %f, roll = %f, pitch = %f, yaw = %f", data.xPos, data.yPos, data.depth, data.roll, data.pitch, data.yaw)
+
+    global xPos, yPos, depth, isSettingPosition
+    xPos = data.xPos
+    yPos = data.yPos
+    depth = data.depth
+    isSettingPosition = 1
+
+
+def setVelocity_callback(data):
+    rospy.loginfo(rospy.get_caller_id()+"I heard setVelocity surgeSpeed = %f, swaySpeed = %f, depth = %f, roll = %f, pitch = %f, yaw = %f", data.surgeSpeed, data.swaySpeed, data.depth, data.roll, data.pitch, data.yaw)
+
+    global surgeSpeed, swaySpeed, depth, isSettingPosition
+    surgeSpeed = data.surgeSpeed
+    swaySpeed = data.swaySpeed
+    depth = data.depth
+    isSettingPosition = 0
     
+
+
 def listener():
+    rospy.init_node('controls', anonymous=True)
 
-    # in ROS, nodes are unique named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The 
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaenously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("autonomy/setPosition", SetPosition, setPosition_callback)
+    rospy.Subscriber("autonomy/setVelocity", SetVelocity, setVelocity_callback)
 
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
+    #rospy.spin()
 
 if __name__ == '__main__':
     listener()
+
+    r = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("isSettingPosition: %d, xPos is: %f, yPos is: %f, depth is: %f, surgeSpeed is: %f, swaySpeed is: %f", isSettingPosition, xPos, yPos, depth, surgeSpeed, swaySpeed)
+
+
+        r.sleep()
+
+
