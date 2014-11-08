29d46d9a3fa3576105aec1b1cb866913b2267c08
Russell Buchanan <russellaabuchanan@gmail.com>
2014-11-08 16:12:09 -0500

    Add PID for all 6 deg freedom

--- catkin_ws/src/controls/scripts/controls.py
+++ catkin_ws/src/controls/scripts/controls.py
@@ -8,84 +8,115 @@ from auv_msgs.msg import SetVelocity
 
 wrenchPublisher = None
 
+#error relative to object
 xPos = 0.0
 yPos = 0.0
 
+#error relative to floating frame
+depth = 0.0
+
+#error relative to horizon
+roll = 0.0
+pitch = 0.0
+
+#error relative to object
+yaw = 0.0
+
+#absolute value
 surgeSpeed = 0.0
 swaySpeed = 0.0
 
-depth = 0.0
-roll = 0.0
-pitch = 0.0
-yaw = 0.0
-
 isSettingPosition = 0
 
-estimated_depth = 0.0
-estimated_pitch = 0.0
 
 def setPosition_callback(data):
     rospy.loginfo(rospy.get_caller_id()+"I heard setPosition xPos = %f, yPos = %f, depth = %f, roll = %f, pitch = %f, yaw = %f", data.xPos, data.yPos, data.depth, data.roll, data.pitch, data.yaw)
 
-    global xPos, yPos, depth, isSettingPosition
+    global xPos, yPos, depth, roll, pitch, yaw, isSettingPosition
     xPos = data.xPos
     yPos = data.yPos
     depth = data.depth
+    roll = data.roll
+    pitch = data.pitch
+    yaw = data.yaw
+
     isSettingPosition = 1
 
 
 def setVelocity_callback(data):
     rospy.loginfo(rospy.get_caller_id()+"I heard setVelocity surgeSpeed = %f, swaySpeed = %f, depth = %f, roll = %f, pitch = %f, yaw = %f", data.surgeSpeed, data.swaySpeed, data.depth, data.roll, data.pitch, data.yaw)
 
-    global surgeSpeed, swaySpeed, depth, isSettingPosition
+    global surgeSpeed, swaySpeed, depth, roll, pitch, yaw, isSettingPosition
     surgeSpeed = data.surgeSpeed
     swaySpeed = data.swaySpeed
     depth = data.depth
+    roll = data.roll
+    pitch = data.pitch
+    yaw = data.yaw
+
     isSettingPosition = 0
     
-def getDepth_callback(data):
-    rospy.loginfo(rospy.get_caller_id()+"I heard estimated_depth = %f", data.data)
-
-    global estimated_depth
-    estimated_depth = data.data
-
 
 def rosInit():
     rospy.init_node('controls', anonymous=True)
 
     global wrenchPublisher
-    rospy.Subscriber("autonomy/set_position", SetPosition, setPosition_callback)
-    rospy.Subscriber("autonomy/set_velocity", SetVelocity, setVelocity_callback)
-    rospy.Subscriber("state_estimation/filteredDepth", Float64, getDepth_callback)
-
     wrenchPublisher = rospy.Publisher("controls/wrench", Wrench, queue_size=100)
 
-    #rospy.spin()
 
 if __name__ == '__main__':
     rosInit()
 
     r = rospy.Rate(1)
 
+    ei_xPos = 0.0
+    ed_xPos = 0.0
 
-    ep_depth = 0.0
+    ei_yPos = 0.0
+    ed_yPos = 0.0
+
     ei_depth = 0.0
     ed_depth = 0.0
 
-    ep_pitch = 0.0
+    ei_roll = 0.0
+    ed_roll = 0.0
+
     ei_pitch = 0.0
     ed_pitch = 0.0
 
-    prev_ep_depth = 0.0
-    prev_ep_pitch = 0.0
+    ei_yaw = 0.0
+    ed_yaw = 0.0
 
-    kp_depth = 1.0
-    ki_depth = 1.0
-    kd_depth = 1.0
+    prev_ep_xPos = 0.0
+    prev_ep_yPos = 0.0
+    prev_ep_depth = 0.0
+    prev_ep_roll = 0.0
+    prev_ep_pitch = 0.0
+    prev_ep_yaw = 0.0
+
+    kp_xPos = 0.0
+    ki_xPos = 0.0
+    kd_xPos = 0.0
+
+    kp_yPos = 0.0
+    ki_yPos = 0.0
+    kd_yPos = 0.0
+
+    kp_depth = 0.0
+    ki_depth = 0.0
+    kd_depth = 0.0
 
     kp_pitch = 0.0
     ki_pitch = 0.0
     kd_pitch = 0.0     
+
+    kp_roll = 0.0
+    ki_roll = 0.0
+    kd_roll = 0.0 
+
+    kp_yaw = 0.0
+    ki_yaw = 0.0
+    kd_yaw = 0.0 
 
     fx = 0.0
     fy = 0.0
@@ -93,40 +124,74 @@ if __name__ == '__main__':
 
     tx = 0.0
     ty = 0.0
-    tz =0.0
+    tz = 0.0
 
 
     dt = 0.1
     r = rospy.Rate(1/dt)
 
     while not rospy.is_shutdown():
-        #rospy.loginfo("isSettingPosition: %d, xPos is: %f, yPos is: %f, depth is: %f, surgeSpeed is: %f, swaySpeed is: %f", isSettingPosition, xPos, yPos, depth, surgeSpeed, swaySpeed)
-        rospy.loginfo("estimated_pitch is: %f", estimated_pitch)
+
+    	rospy.Subscriber("autonomy/set_position", SetPosition, setPosition_callback)
+    	rospy.Subscriber("autonomy/set_velocity", SetVelocity, setVelocity_callback)
+
+    	rospy.loginfo("pitch is: %f, yaw is: %f, surgeSpeed is: %f, swaySpeed is: %f", pitch, yaw, surgeSpeed, swaySpeed,)
+
+    	if(isSettingPosition == 1):
+    		pass
+
+      	  #X position PID control: Taken out for testing with Asimov
+       	 # prev_ep_xPos = xPos
+        	# ei_xPos += xPos*dt
+        	# ed_xPos = (xPos - prev_ep_xPos)/dt
+        	# fx = kp_xPos*xPos + ki_xPos*ei_xPos + kd_xPos*ed_xPos
+
+        	#Y position PID control: Taken out for testing with Asimov
+        	# prev_ep_yPos = yPos
+       		# ei_yPos += yPos*dt
+        	# ed_yPos = (yPos - prev_ep_yaw)/dt
+        	# fy = kp_yPos*yPos + ki_yPos*ei_xPos + kd_yPos*ed_yPos
+
+        else:
+        	fx = 5*surgeSpeed
+        	fy = 5*swaySpeed
+
+        #Depth PID control: Taken out while the dpeth sensor is broken
+        # prev_ep_depth = depth
+        # ei_depth += depth*dt
+        # ed_depth = (depth - prev_ep_depth)/dt
+        # fz = kp_depth*depth + ki_depth*ei_depth + kd_depth*ed_depth
+
+        # Roll PID control: Taken out for test on Asimov who can't control roll
+        # prev_ep_roll = roll
+        # ei_roll += roll*dt
+        # ed_roll = (roll - prev_ep_roll)/dt
+        # tx = kp_roll*roll + ki_roll*ei_roll + kd_roll*ed_roll
+
+        #Pitch PID control
+        prev_ep_pitch = pitch
+        ei_pitch += pitch*dt
+        ed_pitch = (pitch - prev_ep_pitch)/dt
+        ty = kp_pitch*pitch + ki_pitch*ei_pitch + kd_pitch*ed_pitch
+
+        #Yaw PID control
+        prev_ep_yaw = yaw
+        ei_yaw += yaw*dt
+        ed_yaw = (yaw - prev_ep_yaw)/dt
+        tz = kp_yaw*yaw + ki_yaw*ei_yaw + kd_yaw*ed_yaw
 
 
-        #Pitch closed loop control
-        prev_ep_pitch = ep_pitch
-        ep_pitch = pitch - estimated_pitch
-        ei_pitch += ep_pitch*dt
-        ed_pitch = (ep_pitch - prev_ep_pitch)/dt
-        ty = kp_pitch*ep_pitch + ki_pitch*ei_pitch + ki_pitch*ei_pitch
 
 
-        #depth is taken out while the dpeth sensor is broken
-        # prev_ep_depth = ep_depth
-        # ep_depth = depth - estimated_depth
-        # ei_depth += ep_depth*dt
-        # ed_depth = (ep_depth - prev_ep_depth)/dt
-        # fz = kp_depth*ep_depth + ki_depth*ei_depth + kd_depth*ed_depth
         
         wrenchMsg = Wrench()
 
-        wrenchMsg.force.x = 0;
-        wrenchMsg.force.y = 0;
+        wrenchMsg.force.x = fx;
+        wrenchMsg.force.y = fy;
         wrenchMsg.force.z = fz;
-        wrenchMsg.torque.x = 0;
+        wrenchMsg.torque.x = tx;
         wrenchMsg.torque.y = ty;
-        wrenchMsg.torque.z = 0;
+        wrenchMsg.torque.z = tz;
 
         wrenchPublisher.publish(wrenchMsg)
 
