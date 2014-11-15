634427d83ca6b99bea5ae3152e274ff2a4767115
Max Krogius <maxkrogius@gmail.com>
2014-11-15 16:32:05 -0500

    Replaced tabs with spaces

--- catkin_ws/src/autonomy/scripts/test_task.py
+++ catkin_ws/src/autonomy/scripts/test_task.py
@@ -10,18 +10,18 @@ import task_controller
 from abc import ABCMeta, abstractmethod
 
 class Test_Task(task.Task):
-	def __init__(self, phase, my_autonomy):
-		self.phase = phase
-		self.my_autonomy = my_autonomy
+  def __init__(self, phase, my_autonomy):
+    self.phase = phase
+    self.my_autonomy = my_autonomy
 
 
-	def execute(self):
-		if(self.phase == 1):
-			self.phase1()
+  def execute(self):
+    if(self.phase == 1):
+      self.phase1()
 
-	def phase1(self):
-		desired = [3, 0, 0, 0, 0, 0]
-		while(True):
-			#desired[0] = self.my_autonomy.filtered_depth
-			self.my_autonomy.set_velocity(desired)
-			rospy.sleep(1)
+  def phase1(self):
+    desired = [3, 0, 0, 0, 0, 0]
+    while(True):
+      #desired[0] = self.my_autonomy.filtered_depth
+      self.my_autonomy.set_velocity(desired)
+      rospy.sleep(1)
