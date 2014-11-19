5601407c60052bb03a60b9344337d57b06c56e14
Max Krogius <maxkrogius@gmail.com>
2014-11-19 17:44:34 -0500

    Did work on frames for controls

--- catkin_ws/src/state_estimation/tf/src/tf_broadcaster.cpp
+++ catkin_ws/src/state_estimation/tf/src/tf_broadcaster.cpp
@@ -9,95 +9,49 @@
 
 tf::Vector3 zero(0,0,0);
 
-void broadcastStaticFrames() {
-  tf::TransformBroadcaster broadcaster;
-  /*broadcaster.sendTransform(
-    tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 1), 
-    tf::Vector3(0.297, 0.1435, 0.1225)), ros::Time::now(), "/robot", "/robot/rotation_center")
-  );*/
-
-
-  tf::Quaternion imuInternalHorizonToMountPoint;
-  imuInternalHorizonToMountPoint.setRPY(PI/2,0,0);
-  broadcaster.sendTransform(tf::StampedTransform(
-    tf::Transform(imuInternalHorizonToMountPoint, zero), 
-    ros::Time::now(), 
-    "/IMU/initial_horizon", 
-    "/IMU/mount"
-  ));
-  
-  // TODO: Is there a quaternion constructor that combines these two lines
-  tf::Quaternion imuMountToRobotFrame;
-  imuMountToRobotFrame.setRPY(PI/2,0,0);
-
-  tf::Transform imuMountToRobotTransform(imuMountToRobotFrame, tf::Vector3(0., 0., 0.));
-  
-
-  broadcaster.sendTransform(tf::StampedTransform(
-    imuMountToRobotTransform, 
-    ros::Time::now(), 
-    "/IMU", 
-    "/robot"
-  ));
-  broadcaster.sendTransform(tf::StampedTransform(
-    imuMountToRobotTransform, 
-    ros::Time::now(), 
-    "/IMU/mount", 
-    "/robot/initial_horizon"
-  ));
-
-  
-  
-}
-
 void imuCallBack(const geometry_msgs::PoseStamped::ConstPtr& msg) {
   ros::Time time(ros::Time::now());
   tf::TransformBroadcaster broadcaster;
 
-  tf::Quaternion orientation = tf::Quaternion(
+  tf::Quaternion orientation(
     msg->pose.orientation.x, 
     msg->pose.orientation.y, 
     msg->pose.orientation.z, 
     msg->pose.orientation.w
   );
 
+  tf::Quaternion imuMountToRobotFrame;
+  imuMountToRobotFrame.setRPY(PI/2,0,0);
 
-  // orientation is the rotation from the initial_horizon frame of the imu to the imu
-  // and so orientation = Rx(roll)*Ry(pitch)*Rz(yaw)*R(imu,robot)
+  tf::Quaternion imuInternalHorizonToMountPoint;
+  imuInternalHorizonToMountPoint.setRPY(PI/2,0,0);
 
-  broadcaster.sendTransform(
-    tf::StampedTransform(
-      tf::Transform(orientation, zero),
+  tf::Quaternion initialHorizonToRobot;
+  initialHorizonToRobot = imuMountToRobotFrame.inverse() * imuInternalHorizonToMountPoint.inverse() * 
+                          orientation.inverse()*imuMountToRobotFrame;
+ 
+
+  broadcaster.sendTransform(tf::StampedTransform(
+      tf::Transform(initialHorizonToRobot, zero),
       time,
-      "/IMU",
-      "/IMU/initial_horizon"   
-    )
-  );
+      "/initial_horizon",
+      "/robot"   
+  ));
 
-  /*
-  * This doesn't work because of tf and threading
-  tf::Transformer transformer;
-  if (transformer.canTransform("/robot/initial_horizon", "/robot", time)) {
-  tf::StampedTransform robotTransform;
-  double roll,pitch,yaw;
   
-  transformer.lookupTransform("/robot/initial_horizon", "/robot", time, robotTransform);
-  robotTransform.getBasis().getRPY(roll,pitch,yaw);
+  double roll,pitch,yaw;
+  tf::Matrix3x3(initialHorizonToRobot).getRPY(roll,pitch,yaw);
 
   tf::Quaternion yawQuat;
   yawQuat.setRPY(0,0,yaw);
   broadcaster.sendTransform(tf::StampedTransform(
     tf::Transform(yawQuat, zero),
     time,
-    "/robot/initial_horizon",
-    "/robot/horizon"
-  ));*/
+    "/initial_horizon",
+    "/horizon"
+  ));
   
 
-}
-
-void timerCallback(const ros::TimerEvent& event) {
-  broadcastStaticFrames();
 }
 
 int main(int argc, char** argv) {
@@ -106,8 +60,6 @@ int main(int argc, char** argv) {
 
   // TODO: Figure out why nothing gets broadcast without this line
   tf::TransformBroadcaster broadcaster;
-  // TODO: Do we need this on a timer?
-  ros::Timer timer = node.createTimer(ros::Duration(0.1), timerCallback);
   ros::Subscriber imuSub = node.subscribe("state_estimation/pose", 1000, imuCallBack);
   ros::spin();
 
