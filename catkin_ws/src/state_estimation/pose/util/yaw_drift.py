64302e0c573bf70ea6a132040feb3fc1f804d420
cipherera <harrycaiao@hotmail.com>
2014-11-26 17:06:59 -0500

    yaw_drift.py now calculates yaw drift of every 5000 messages - in progress

--- catkin_ws/src/state_estimation/pose/util/yaw_drift.py
+++ catkin_ws/src/state_estimation/pose/util/yaw_drift.py
@@ -1,10 +1,18 @@
 import rosbag
 import math
 bag = rosbag.Bag('half_hour_pose.bag')
-msg=0
-angle = 0
+n=0
+initial_angle = 0
+final_angle = 0
 for topic, msg, t in bag.read_messages(topics=["/state_estimation/pose"]):
-        angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
-        print angle
+        if n == 0:
+        	initial_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
+		n+=1
+	elif n == 5000:
+        	final_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
+        	print "change in angle is " + repr(final_angle - initial_angle) + " from time x to y.(settakes change in yaw every 5000 messages at the moment)" 
+		n=0
+	else:
+		n+=1
 bag.close()
 
