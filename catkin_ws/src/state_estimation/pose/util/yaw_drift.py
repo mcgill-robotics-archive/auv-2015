bfc1eb076226e35c8a4c7d094a97e4ab0613cffa
cipherera <harrycaiao@hotmail.com>
2014-11-26 17:46:26 -0500

    completed yaw_drift.py to find yaw drift for every 40 second interval

--- catkin_ws/src/state_estimation/pose/util/yaw_drift.py
+++ catkin_ws/src/state_estimation/pose/util/yaw_drift.py
@@ -4,13 +4,19 @@ bag = rosbag.Bag('half_hour_pose.bag')
 n=0
 initial_angle = 0
 final_angle = 0
+initial_time = 0
+final_time = 0
+
 for topic, msg, t in bag.read_messages(topics=["/state_estimation/pose"]):
+
         if n == 0:
         	initial_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
+		initial_time = t.secs
 		n+=1
-	elif n == 5000:
+	elif n == 10000:
         	final_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
-        	print "change in angle is " + repr(final_angle - initial_angle) + " from time x to y.(settakes change in yaw every 5000 messages at the moment)" 
+		final_time = t.secs
+        	print repr(final_angle - initial_angle) + " in " + repr(final_time - initial_time) + " seconds"
 		n=0
 	else:
 		n+=1
