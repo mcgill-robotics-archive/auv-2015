fc36ba84ec02b779ad43ba7b667a51ff2e7e3883
unknown <aimee_16_94@hotmail.com>
2014-11-30 16:57:23 -0500

    depth graph using highcharts

--- nodejs/public/components/Aimee/depth-graph/depth-graph.js
+++ nodejs/public/components/Aimee/depth-graph/depth-graph.js
@@ -1,26 +1,80 @@
-Polymer('aimee-card', {
-
-    ready: function(){
-        this.dataset =[];
-        this.values.forEach(function(val,i){
-            this.dataset.push({
-                //fillColor: "rgba("+this.color[i]+",0.5)",
-                strokeColor: "rgba("+this.color[i]+",1)",
-                pointColor: "rgba("+this.color[i]+",1)",
-                data: this.values[i]
+Polymer('depth-graph', {
+    $(function () {
+        $(document).ready(function () {
+            Highcharts.setOptions({
+                global:{
+                    useUTC: false
+                }
             });
-        },this);
 
-        this.data= {labels: this.labels, dataset: this.datasets};
-        console.log(this.data);
+            $('container').highcharts({
+                chart:{
+                    type: 'spline', //conect points with a curve
+                    animation: Highcharts.svg,
+                    marginRight: 10,
+                    events:{
 
-        this.ctx=this.$.canvas.getContext('2d');
-        this.chart= new Chart(this.ctx).Line(this.data);
-    },
+
+                        //load new values to the chart at an interval of time
+                        load: function () {
+                            var series = this.series[0];//an array with x and y values
+                            setInterval(function(){
+                                var x =(new Date()).getTime(), //current time
+                                    y = Math.random(); //generate random number for y value
+                                series.addPoint([x,y], true,true); //add point to the
+                            }, 1000);//update chart every 1 second
 
 
-    labels:["a", "b", "c"],
-    values:[1,2,3],
-    colors:[],
+                        }
+                    }
+                },
 
+                //setup of the graph
+                title: {
+                    text:' '
+                },
+                xAxis:{
+                    type: 'time',
+                    tickPixelInterval:150
+                },
+                yAxis:{
+                    title:{
+                        text:'value'
+                    },
+                    plotlines:[{
+                        value:0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+
+                },
+                tooltip:{
+                    enabled:false
+                },
+
+                legend:{
+                    enabled:false
+                },
+                exporting:{
+                    enabled:false
+                },
+                series:[{
+                    name: 'data',
+                    data:(function(){
+                        //Input of initial data (20 pts)
+                        var data=[],
+                            time=(new Date()).getTime(),
+                            i;
+                        for (i=-19; i<=0;i++){
+                            data.push({
+                                x:time+i*1000,
+                                y:Math.random()
+                            });
+                        }
+                        return data;
+                    }())
+                }]
+            });
+        });
+    });
 });
