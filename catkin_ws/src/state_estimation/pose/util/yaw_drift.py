49f2dec09d4b74021f1eaf411b16b571a7316cd7
cipherera <harrycaiao@hotmail.com>
2014-12-04 14:54:07 -0500

    updated yaw_drift.py to calculate mean and standard deviation of yaw drift

--- catkin_ws/src/state_estimation/pose/util/yaw_drift.py
+++ catkin_ws/src/state_estimation/pose/util/yaw_drift.py
@@ -1,24 +1,34 @@
 import rosbag
 import math
+import numpy
 bag = rosbag.Bag('half_hour_pose.bag')
 n=0
 initial_angle = 0
 final_angle = 0
 initial_time = 0
 final_time = 0
-
+data_points = []
+number_of_terms = 0
 for topic, msg, t in bag.read_messages(topics=["/state_estimation/pose"]):
 
         if n == 0:
-        	initial_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
+        	initial_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w) 
 		initial_time = t.secs
 		n+=1
 	elif n == 10000:
         	final_angle = 2*math.atan2(math.sqrt(math.pow(msg.pose.orientation.x, 2) + math.pow(msg.pose.orientation.y, 2) + math.pow(msg.pose.orientation.z, 2)), msg.pose.orientation.w)
 		final_time = t.secs
-        	print repr(final_angle - initial_angle) + " in " + repr(final_time - initial_time) + " seconds"
+		yaw_drift = final_angle - initial_angle
+		time_interval = final_time - initial_time
+		data_points.append(yaw_drift)
+		number_of_terms+=1
+        	print repr(yaw_drift) + " in " + repr(time_interval) + " seconds"
 		n=0
 	else:
 		n+=1
 bag.close()
+
+print "mean is " + repr(numpy.mean(data_points))
+print "standard deviation is " + repr(numpy.std(data_points))
+
 
