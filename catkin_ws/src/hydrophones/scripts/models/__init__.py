665d6f47e5d11649e24e5fa28169e1ae8bf22e68
Anass Al <mr@anassinator.com>
2014-12-24 16:53:17 +0300

    Add Mic model

---
+++ catkin_ws/src/hydrophones/scripts/models/__init__.py
@@ -0,0 +1,6 @@
+"""Hydrophone task models."""
+
+__author__ = "Anass Al-Wohoush"
+__version__ = "1.0"
+
+from mic import mics, NUMBER_OF_MICS
---
+++ catkin_ws/src/hydrophones/scripts/models/mic.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Microphone model."""
+
+__author__ = "Anass Al-Wohoush"
+__version__ = "1.0"
+
+import os
+import rospy
+
+
+class Mic(object):
+
+    """Microphone properties.
+
+    Microphone IDs range from 0 (origin) to the number of microphones in the
+    array exclusively. Origin transducer is placed at (0,0).
+
+    Attributes:
+        id: Mic id (int).
+        x: X coordinate relative to origin transducer in meters.
+        y: Y coordinate relative to origin transducer in meters.
+    """
+
+    PARAM_PATH = "/hydrophones/mic/"
+
+    def __init__(self, id):
+        """Construct Mic object.
+
+        Args:
+            id: Mic id (int).
+        """
+        self.id = id
+        self._path_x = os.path.join(Mic.PARAM_PATH, str(self.id), 'x')
+        self._path_y = os.path.join(Mic.PARAM_PATH, str(self.id), 'y')
+
+    def __repr__(self):
+        """Return string representation of transducer."""
+        return "<Mic x: {mic.x:+4.2f} m y: {mic.y:+4.2f} m>".format(mic=self)
+
+    @property
+    def x(self):
+        """X coordinate relative to origin transducer in meters.
+
+        Raises:
+            KeyError: ROS parameter is not set.
+        """
+        return float(rospy.get_param(self._path_x))
+
+    @x.setter
+    def x(self, value):
+        """Set X coordinate in meters."""
+        rospy.set_param(self._path_x, float(value))
+
+    @property
+    def y(self):
+        """Y coordinate relative to origin transducer in meters.
+
+        Raises:
+            KeyError: ROS parameter is not set.
+        """
+        return float(rospy.get_param(self._path_y))
+
+    @y.setter
+    def y(self, value):
+        """Set Y coordinate in meters."""
+        rospy.set_param(self._path_y, float(value))
+
+
+# Microphone array
+NUMBER_OF_MICS = 4
+mics = [Mic(id) for id in range(NUMBER_OF_MICS)]
