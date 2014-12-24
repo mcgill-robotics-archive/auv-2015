96caf6741c7f832052658ef910116ef140f77c7b
Anass Al <mr@anassinator.com>
2014-12-24 17:20:32 +0300

    Add Audio model

--- catkin_ws/src/hydrophones/scripts/models/__init__.py
+++ catkin_ws/src/hydrophones/scripts/models/__init__.py
@@ -4,4 +4,5 @@ __author__ = "Anass Al-Wohoush"
 __version__ = "1.0"
 
 from env import env
+from audio import audio
 from mic import mics, NUMBER_OF_MICS
---
+++ catkin_ws/src/hydrophones/scripts/models/audio.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Audio model."""
+
+__author__ = "Anass Al-Wohoush"
+__version__ = "1.0"
+
+import os
+import rospy
+
+
+class Audio(object):
+
+    """Incoming audio stream properties.
+
+    Attributes:
+        buffersize: Buffersize of incoming audio stream.
+        fs: Sampling frequency in Hz.
+    """
+
+    PARAM_PATH = "/hydrophones/audio/"
+
+    @property
+    def buffersize(self):
+        """Buffersize of incoming audio stream.
+
+        Raises:
+            KeyError: ROS parameter is not set.
+        """
+        path = os.path.join(Audio.PARAM_PATH, "buffersize")
+        return int(rospy.get_param(path))
+
+    @buffersize.setter
+    def buffersize(self, value):
+        """Set buffersize of incoming audio stream."""
+        path = os.path.join(Audio.PARAM_PATH, "buffersize")
+        rospy.set_param(path, int(value))
+
+    @property
+    def fs(self):
+        """Sampling frequency in Hz.
+
+        Raises:
+            KeyError: ROS parameter is not set.
+        """
+        path = os.path.join(Audio.PARAM_PATH, "fs")
+        return float(rospy.get_param(path))
+
+    @fs.setter
+    def fs(self, value):
+        """Set sampling frequency in Hz."""
+        path = os.path.join(Audio.PARAM_PATH, "fs")
+        rospy.set_param(path, float(value))
+
+
+audio = Audio()
