3d3f66cba8e8e3a5abcbf2645046c57d9b2b7fd4
Henry Yu <mrhenryyu@hotmail.com>
2014-12-26 21:37:16 -0500

    Added random point generator and backend data passing (not complete)

--- nodejs/public/components/map/mapcanvas.js
+++ nodejs/public/components/map/mapcanvas.js
@@ -34,30 +34,76 @@ window.onload = function () {
         gridThickness:0.8};
     
 //creating the first object 
-    var object1 = {
+    /*var object1 = {
         type: "scatter",  
         toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span> <br/> <strong>Y-Position</strong> {y} <br/> <strong>X-Position</strong> {x} ",
         name: "Object 1",
         showInLegend: true
-    };
+    };*/
+    
+//data points 
+    var dps = [{
+        x:30,
+        y:40,
+        name:"initial"
+    }]; 
+    chart.options.data = [{
+        type: "scatter",
+        toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span> <br/> <strong>Y-Position</strong> {y} <br/> <strong>X-Position</strong> {x} ",
+        dataPoints:dps 
+			}];
+		//});
 //pushing the data into the chart
-    chart.options.data = [];
+    /*chart.options.data = [];
     chart.options.data.push(object1);
     object1.dataPoints = [
-        { x: 51, y: 10,name:"0ms" },
-        { x: 42, y: 20,name:"10ms" },
+        { x: 51, y: 10,name:"initial" }*/
+        /*{ x: 42, y: 20,name:"10ms" },
         { x: 30, y: 55,name:"20ms" },
         { x: 12, y: -10,name:"30ms" },
 		{ x: 29, y: -50,name:"40ms" },
 		{ x: -20, y: 98,name:"50ms" },
 		{ x: 16, y: -30,name:"60ms" },
-		{ x: 9, y: 0,name:"70ms" },
-        { x: -80, y: 0,name:"80ms" }
-        ];
+		{ x: 9, y: 0,name:"70ms" },*/
+       
+        //];
 
-    var xvalue = document.getElementById("xvalue");
-    var yvalue = document.getElementById("yvalue");
+    //rendering the chart when the page loads 
+        chart.render();
+
+    var newX;
+    var newY;
+    //after window load
+  
+    //Math.floor((Math.random() * 100) + 1) returns a number between 1 and 100
+    //create a function that spews out numbers every second
+    function timedInput() {
+        window.setInterval(function(){
+            newX = Math.floor((Math.random() * 100) + 1);
+            console.log("newX is " + newX);
+            newY = Math.floor((Math.random() * 100) + 1);
+            console.log("newY is " + newY);
+        },1000);
+    }
+    timedInput();
+
+    //set the graph to "check" the value and change it after 5 seconds 
+    setTimeout(function(){
+       
+        dps[0].push({
+        x:50,
+        y:50,
+        name:"change"
+    });
+       /* dps[0].x=Math.floor((Math.random() * 100) + 1);
+        dps[0].y=Math.floor((Math.random() * 100) + 1);
+        dps[0].name= "changed";*/
+        
+        chart.render;
+        console.log("It worked!");
+        console.log(dps[0].x);
+        console.log(dps[0].y);
+        console.log(dps[0].name);
+    },5000);
     
-//rendering the chart
-    chart.render();
 }
