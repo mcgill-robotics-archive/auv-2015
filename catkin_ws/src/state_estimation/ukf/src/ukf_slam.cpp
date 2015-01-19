1cb1845215fa4a76b832da2fead3840274b1e727
Nicole <nicole.ayalon@mail.mcgill.ca>
2015-01-19 13:03:13 -0800

    New file...

---
+++ catkin_ws/src/state_estimation/ukf/src/ukf_slam.cpp
@@ -0,0 +1,22 @@
+#include "ukf.h"
+#include <math.h>
+#include <stdio.h>
+
+
+Vector2d testInput(x,y);
+
+
+void SLAM(Vector2D testInput){
+
+//call constructor on vector of 2n dimensions
+//ukf slam will look like ukf pose
+//1.cosntructor creates ukf of size 2n
+//2.update will take in sensor readings
+// observe-=-"h" 
+
+//3.
+
+
+
+
+}
