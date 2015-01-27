32fb015f136dd2f1d018a28f3206454f1ad57a00
RiIakkuma <bhsulin@hotmail.com>
2015-01-27 14:53:34 -0500

    changed the name of files and methods

--- catkin_ws/src/autonomy/scripts/path_task.py
+++ catkin_ws/src/autonomy/scripts/path_task.py
@@ -3,7 +3,8 @@
 import task
 import forward_movement_action
 import set_cv_target
-
+import get_transforms
+ 
 class PathTask(tast.Task:
 	def __init__(self, phase, my_autonomy):
 		self.task_name = "Path"
@@ -20,7 +21,7 @@ class PathTask(tast.Task:
 	def load_action_stack(self):
 		if (self.phase > 0):
 			my_path_colour = set_cv_target.SetBallAction(self.my_autonomy, pathColor)
-			my_position = set_se_transforms.SetTransforms(self.my_autonomy, position)
+			my_position = get_transforms.GetTransforms(self.my_autonomy, Object)
 			my_forward_movement_action = forward_movement_action.ForwardMovementAction(self.my_autonomy, 3)
 			self.action_stack.insert(0, my_path_colour)
 			self.action_stack.insert(1, my_position)
