228b30bba238320ca9869980353bfba62f06c864
scsewell <scott.sewell@mail.mcgill.ca>
2015-02-23 16:22:17 -0500

    Refactor of code to better allow for PyPipe integration

--- catkin_ws/src/computer_vision/src/computer_vision_COLOR.cpp
+++ catkin_ws/src/computer_vision/src/computer_vision_COLOR.cpp
@@ -13,15 +13,18 @@
 #include <computer_vision/object_finder.hpp>
 
 // GUI Options
-static const std::string IMAGE_WINDOW = "Image";
-static const bool SHOW_IMAGE = true;
 static const bool SHOW_HISTOGRAM = true;
 static const bool SHOW_THRESHOLDS = false;
 static const bool SHOW_BOUNDING_BOXS = true;
 static const float HISTOGRAM_EXPONENT_SCALE = 0.25f; // changes the exponent that changes the histogram scale [0,1]
 
+// Filters (TEMPORARY)
+static const bool USE_GUASSIAN_BLUR = false;
+static const int GUASSIAN_BLUR_SIZE = 15; // size of the guassian blur, should be odd
+static const bool USE_MEDIAN_BLUR = true;
+static const int MEDIAN_BLUR_SIZE = 13; // size of the median blur, should be odd
+
 // Filtering Options
-static const int BLUR_SIZE = 15; // size of the guassian blur
 static const float MIN_PEAK_THRESHOLD = 0.005f; // the minimum fraction of the image a single hue must contain to be recognised as a peak [0,1]
 static const float MAX_PEAK_THRESHOLD = 0.20f; // the maximum fraction of the image a single hue can contain to be recognised as a peak (for ignoring a background color) [0,1]
 static const float MIN_PEAKINESS = 0.25f; // the minimum prominence of a hue peak required to be thresholded [0,1]
@@ -54,46 +57,62 @@ public:
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr img_ptr;
-    cv::Mat img_rgb, img_hsv;
 
     try
     {
       img_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      img_rgb = img_ptr->image.clone();
+      processImageColor(img_ptr->image.clone());
     }
     catch (cv_bridge::Exception& e)
     {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+  }
 
+  /*
+  * Takes in a rgb image from the pipeline and runs color based analysis on it.
+  * 
+  * Currently has no return, but eventually it should return the origional image
+  * back into the pipe, along with a stuct containing data about visible objects.
+  */
+  void processImageColor(cv::Mat img_rgb) 
+  {
+    //---------------------MOVE THESE FILTERS INTO PIPELINE------------------
+    if (true)
+    {
+      cv::imshow("Input Image", img_rgb);
+      cv::namedWindow("Input Image");
+      cv::waitKey(3);
+    }
+    // clean image for analysis
     cv::normalize(img_rgb, img_rgb, 0, 255, cv::NORM_MINMAX, CV_8UC1); // maximizes the contrast
-    cv::GaussianBlur(img_rgb, img_rgb, cv::Size(BLUR_SIZE, BLUR_SIZE), 0, 0, cv::BORDER_DEFAULT); // Reduces noise
+    
+    if (USE_GUASSIAN_BLUR) 
+    {
+      cv::GaussianBlur(img_rgb, img_rgb, cv::Size(GUASSIAN_BLUR_SIZE, GUASSIAN_BLUR_SIZE), 0, 0, cv::BORDER_DEFAULT); // Reduces noise
+    } 
+    if (USE_MEDIAN_BLUR) 
+    {
+      cv::medianBlur(img_rgb, img_rgb, MEDIAN_BLUR_SIZE); // Reduces noise
+    }
+    //-----------------------------------------------------------------------
 
+    cv::Mat img_hsv; 
     cvtColor(img_rgb, img_hsv, CV_BGR2HSV); // Convert from BGR to HSV
 
-    // Create hue histogram
+    // Analyze hue histogram
     hist.createHistogram(img_hsv);
     hist.findPeakHues(MIN_PEAK_THRESHOLD, MAX_PEAK_THRESHOLD, MIN_PEAKINESS, img_hsv.rows * img_hsv.cols);
     std::list<HuePeak> hues = hist.getHuePeaks();
     
     std::list<cv::Mat> imgs_threshold = threshold.threshold(img_hsv, hues, S_MIN, V_MIN); // Threshold images
 
-    // Finds the different objects of each color
+    // Find the different objects of each color
     objectFinder.findObjects(imgs_threshold, hues, MIN_AREA, MIN_LINE_LENGTH_RATIO);
     std::list<VisibleObject> visibleObjects = objectFinder.getVisibleObjects();
 
-    // Output raw video stream
-    image_pub_.publish(img_ptr->toImageMsg());
-
     // Update GUI Windows
-    if (SHOW_IMAGE) 
-    {
-      cv::imshow(IMAGE_WINDOW, img_ptr->image);
-      cv::namedWindow(IMAGE_WINDOW);
-      cv::waitKey(3);
-    }
-
     if (SHOW_HISTOGRAM) {
       hist.drawHistogram(HISTOGRAM_EXPONENT_SCALE);
     }
