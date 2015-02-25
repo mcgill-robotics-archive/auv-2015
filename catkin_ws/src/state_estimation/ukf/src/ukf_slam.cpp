ed6fdb8d3cc7b1eee2ea1cc4dad609e95e71d95b
Max Krogius <maxkrogius@gmail.com>
2015-02-25 13:50:03 -0500

    Formatting

--- catkin_ws/src/state_estimation/ukf/src/ukf_slam.cpp
+++ catkin_ws/src/state_estimation/ukf/src/ukf_slam.cpp
@@ -10,7 +10,8 @@ ukf_slam::ukf_slam(int n):
 
 
 MatrixXd ukf_slam::observe(MatrixXd sigmas, int objectID) {
-	return sigmas.block(2*objectID,0, 2, sigmas.cols());	//Return 2 by 4n block from sigmas
+  //Return 2 by 4n block from sigmas
+	return sigmas.block(2*objectID,0, 2, sigmas.cols());	
 }	
 
 void ukf_slam::propogate(Ref<Eigen::VectorXd> state) {
@@ -24,6 +25,7 @@ MatrixXd ukf_slam::getCovariance(int objectId) {
 void ukf_slam::update(Vector2d msmt, Ref<VectorXd> outPosition, int objectID)
 {
 	estimator.predict(&propogate, 0.0 * MatrixXd::Identity(2*4,2*4));
-	estimator.correct(msmt, boost::bind(&ukf_slam::observe, _1, objectID), MatrixXd::Identity(2,2));
+	estimator.correct(msmt, boost::bind(&ukf_slam::observe, _1, objectID),
+	    MatrixXd::Identity(2,2));
 	outPosition = estimator.state;
 }
