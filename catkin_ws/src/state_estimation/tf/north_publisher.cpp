c40fa061975ee894a92ee1c21fbc438be2f3c2ed
Nicole Ayalon <nicole.ayalon@gmail.com>
2015-04-01 13:20:13 -0400

    Create north_publisher.cpp
    Started the creating of north-publisher.

---
+++ catkin_ws/src/state_estimation/tf/north_publisher.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+//#include <geometry_msgs/***.h> What's getting included?
+
+int main(int argc, char **argv){
+
+//File is mostly witten on my desktop
+//I'll copy it here
+//VM crashed!!
+
+}
