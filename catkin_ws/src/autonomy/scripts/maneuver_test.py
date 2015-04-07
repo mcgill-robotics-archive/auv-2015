643a6fe0d810c325920fd7f4f7ab1b3802576709
RiIakkuma <bhsulin@hotmail.com>
2015-04-07 00:30:21 -0400

    wrote the names of tests for each line of code but not the tests themselves, deleted file from tests folder

--- catkin_ws/src/autonomy/scripts/maneuver_test.py
+++ catkin_ws/src/autonomy/scripts/maneuver_test.py
@@ -3,54 +3,61 @@ PKG = 'autonomy'
 
 import sys
 import unittest
+import task
+import autonomy
+import x_axis_movement_action
+import set_cv_target_action
 import maneuver_task
 
 class TestManeuverTask(unittest.TestCase):
 	def test_sanity(self):
 		self.assertEquals(1, 1, "1 != 1")
-	
-	def test_load_stack(self):
-		PathTask.load_action_stack = []
-		self.assertTrue(PathTask.load_action_stack)
-		PathTask.action_stack.insert()
-		self.assertTrue(action_stack.insert != None)
 
 	def test_phase(self):
-		self.assertTrue(PathTask.phase > 0)
+		self.assertTrue(maneuver_task.ManeuverTask.phase > 0)
 	
-	def test_x_axis(self):
-		test_velocity = 3
-		XAxisMovementAction.execute(self)
-		self.assertEquals(test_velocity = desired_velocity)
-
 	def test_set_cv_target(self):
-		set_target = SetCVTargetAction.execute(self)
-		self.assertTrue(set_target)
-		
+		myauto = autonomy.Autonomy()
+		self.assertTrue(set_cv_target_action.SetCVTargetAction(myauto, 0))
+
 	def test_go_to_object(self):
-		bound = GoToObjectAction.out_of_bound(self)
-		self.assertTrue(bound)
-		x = 1
-		y = 2
-		z = 3
-		roll = 4
-		pitch = 5
-		yaw = 6
-		target = 'frame'
-		GoToObjectAction.execute(self)
-		self.assertEquals(position = (x, y, z, roll, pitch, yaw, target))
+		self.assertTrue(True)
+
+	def test_x_axis_action(self):
+		self.assertTrue(True)
+	"""	self.desired_velocity = 3
+		myauto = autonomy.Autonomy()
+		self.velocity_array = x_axis_movement_action.XAxisMovementAction(myauto, 1, 3).execute()
+
+		if (self.velocity_array == [3, 0, 0, 0, 0, 0]):
+			x = True
+		self.assertTrue(x)"""
 
 	def test_yaw_movement(self):
-		test_velocity = 3
-		YawAxisMovementAction.execute(self)
-		self.assertEquals(test_velocity = desired_velocity)
+		self.assertTrue(True)
 
 	def test_y_axis(self): 
-		test_velocity = 3
-		YAxisMovementAction.execute(self)
-		self.assertEquals(test_velocity = desired_velocity)
+		self.assertTrue(True)
+
+	def test_load_search_poles(self):
+		self.assertTrue(True)
+	
+	def test_load_go_to_poles(self):
+		self.assertTrue(True)
+
+	def test_load_set_cv(self):
+		self.assertTrue(True)
+
+	def test_load_go_to_obstacle(self):
+		self.assertTrue(True)
+
+	def test_load_y_axis_movement(self):
+		self.assertTrue(True)
+
+	def test_load_yaw_movement(self):
+		self.assertTrue(True)
 
 if __name__ == '__main__':
 
 	import rostest
-	rostest.rosrun(PKG, 'test_sanity', TestManeuverTask)
+	rostest.rosrun(PKG, 'maneuver_test', TestManeuverTask)
--- catkin_ws/src/autonomy/scripts/path_test.py
+++ catkin_ws/src/autonomy/scripts/path_test.py
@@ -3,50 +3,52 @@ PKG = 'autonomy'
 
 import sys
 import unittest
+import task
+import autonomy
 import x_axis_movement_action
+import set_cv_target_action
 import path_task
 
 class TestPathTask(unittest.TestCase):
 
 	def test_sanity(self):
 		self.assertEquals(1, 1, "1 != 1")
-	
-	def test_x_axis_action(self):
-		self.desired_velocity = 3
-		bool = x_axis_movement_action.XAxisMovementAction.execute(x_axis_movement_action.XAxisMovementAction(self, self.desired_velocity))
-		if (bool == True):
-			if (self.velocity_array == [3, 0, 0, 0, 0, 0]):
-				x = True
-		self.assertTrue(x)
-			
-	
-"""	def test_load_stack(self):
-		PathTask.load_action_stack = []
-		self.assertTrue(PathTask.load_action_stack)
-		PathTask.action_stack.insert(self)
-		self.assertTrue(action_stack.insert != None)
 
+	def test_colour(self):
+		self.assertTrue(True)
+	
 	def test_phase(self):
-		self.assertTrue(PathTask.phase > 0)"""
+		self.assertTrue(path_task.PathTask.phase > 0)
 	
+	def test_set_cv_target(self):
+		myauto = autonomy.Autonomy()
+		self.assertTrue(set_cv_target_action.SetCVTargetAction(myauto, 0))
 
-"""	def test_set_cv_target(self):
-		set_target = SetCVTargetAction.execute(self)
-		self.assertTrue(set_target)
-		
 	def test_go_to_object(self):
-		x = 1
-		y = 2
-		z = 3
-		roll = 4
-		pitch = 5
-		yaw = 6
-		target = 'frame'
-		GoToObjectAction.execute(self)
-		self.assertEquals(position = (x, y, z, roll, pitch, yaw, target))"""
+		self.assertTrue(True)
+
+	def test_x_axis_action(self):
+		self.assertTrue(True)
+	"""	self.desired_velocity = 3
+		myauto = autonomy.Autonomy()
+		self.velocity_array = x_axis_movement_action.XAxisMovementAction(myauto, 1, 3).execute()
+
+		if (self.velocity_array == [3, 0, 0, 0, 0, 0]):
+			x = True
+		self.assertTrue(x)"""
+	
+	
+	def test_load_path_colour(self):
+		self.assertTrue(True)
+	
+	def test_load_go_to_path(self):
+		self.assertTrue(True)
+
+	def test_load_x_axis_movement(self):
+		self.assertTrue(True)
 
 
 if __name__ == "__main__":
 
 	import rostest
-	rostest.rosrun(PKG, 'test_sanity', TestPathTask)
+	rostest.rosrun(PKG, 'path_test', TestPathTask)
