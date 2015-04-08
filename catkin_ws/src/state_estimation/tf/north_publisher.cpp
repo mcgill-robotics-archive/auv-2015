236289bf3bc45874c061b1e5f177b232e21d88b5
Nicole Ayalon <nicole.ayalon@gmail.com>
2015-04-08 11:09:09 -0400

    Update north_publisher.cpp
    Updated to include more structure

--- catkin_ws/src/state_estimation/tf/north_publisher.cpp
+++ catkin_ws/src/state_estimation/tf/north_publisher.cpp
@@ -1,10 +1,39 @@
+//Subscribe to magnetometer readings from IMU
+//Check Ximupublisher in state_estimation for details of the magnetometer topic
+//The node should create the tf transform between the tf 
+//frame /initial_horizon and /north. Their definitions are on the wiki
+
 #include <ros/ros.h>
-//#include <geometry_msgs/***.h> What's getting included?
+#include <geometry_msgs/?.h> //Change this
+int main(int argc, char **argv) {
 
-int main(int argc, char **argv){
+    ros::init(argc, argv, "north_publisher");
 
-//File is mostly witten on my desktop
-//I'll copy it here
-//VM crashed!!
+
+
+    ros::NodeHandle n;
+
+
+
+    ros::Subscriber sub = n.subscribe("Ximupublisher", 1000, callbackFunction); //What is actualy name of magnetometer ximupublisher
+
+
+
+    ros::spin();
 
 }
+
+
+
+//callbackFunction gets called everytime the node youre subcsribed to publishes a message
+
+//It suffices that the geometry_msgs::Pose part should be replaced with whatever the return type of the subscribed node is
+
+void callbackFunction(const geometry_msgs::Pose::ConstPtr& msg) {
+
+    ROS_INFO("%f", msg->pose.orientation.x) //change to ximu publisher
+    //Here can use the data however we want
+    //Will be a big function, maybe?
+
+}
+
