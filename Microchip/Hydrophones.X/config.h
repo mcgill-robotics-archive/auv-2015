79eebdc57732ff020d0373e6e6bf3d853731f83b
augustelalande <auguste.lalande@gmail.com>
2015-05-17 22:05:29 -0400

    sample() not working

--- Microchip/Hydrophones.X/config.h
+++ Microchip/Hydrophones.X/config.h
@@ -3,8 +3,10 @@
     #define M 100           // subject to power restrictions
     #define N1 2            // 2 is minimum
     #define N2 2            // 2 is minimum
-    // FCY = ((input clock) * M/(N1 * N2)/2
-    // Equation 9-1, and 9-2 in datasheet
+     /*
+      * FCY = ((input clock) * M/(N1 * N2)/2
+      * Equation 9-1 and 9-2 in datasheet
+      */
     #define FCY INTERNAL_OSCILLATOR * M/(N1 * N2 * 2)
 #endif
 
--- Microchip/Hydrophones.X/main.c
+++ Microchip/Hydrophones.X/main.c
@@ -8,23 +8,28 @@
 #include "util.h"
 //#include "global.h"
 
-extern int **burstBuffer[4];
-extern int **passiveBuffer[4];
-
 int main(void)
 {
     initApp();
-    
+
     while(1) {
-        println("hello");
-        delay_s(1);
+        println(printf("%d,%d,%d,%d,", ADC1BUF1, ADC1BUF2, ADC1BUF3, ADC1BUF0));
+        delay_ms(100);
     }
+
+//    int burstBuffer[4][1000];
+//    //int passiveBuffer[4][1000];
+//
 //    int i = 0;
 //    for(i = 0; i < 1000; i++) {
 //        burstBuffer[0][i] = sample(1);
 //        burstBuffer[1][i] = sample(2);
 //        burstBuffer[2][i] = sample(3);
 //        burstBuffer[3][i] = sample(4);
+//    }
+//
+//    for(i = 0; i < 1000; i++) {
+//        println(printf("%d,%d,%d,%d,", burstBuffer[0][i], burstBuffer[1][i], burstBuffer[2][i], burstBuffer[3][i]));
 //    }
 //
 //    for(i = 0; i < 4; i++) {
