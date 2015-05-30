9d2bfb3f0a9d8dee3906be0cb3f7c46a8f727952
cipherera <harrycaiao@hotmail.com>
2015-05-30 15:03:14 -0400

    added I2C teensy version of depth sensor broadcaster node

---
+++ depth_sensor/depth_sensor_teensyI2C.ino/depth_sensor_teensyI2C.ino.ino
@@ -0,0 +1,41 @@
+#include <SparkFun_MS5803_I2C.h>
+#include <i2c_t3.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+//Set up the ros node and publisher
+std_msgs::Float32 pressure_msg;
+std_msgs::Float32 temperature_msg;
+ros::Publisher pub_pressure("pressure", &pressure_msg);
+//ros::Publisher pub_temperature("temperature", &temperature_msg);
+ros::NodeHandle nh;
+
+MS5803 sensor(ADDRESS_HIGH);
+long publisher_timer;
+
+void setup()
+{
+  Wire1.begin(I2C_MASTER, 0x00, I2C_PINS_29_30, I2C_PULLUP_EXT, I2C_RATE_200);
+  sensor.reset();
+  sensor.begin();  
+  nh.initNode();
+  nh.advertise(pub_pressure);
+
+}
+
+void loop()
+{
+
+  if (millis() > publisher_timer) {
+
+  pressure_msg.data = sensor.getPressure(ADC_4096);
+  temperature_msg.data=sensor.getTemperature(CELSIUS, ADC_1024);
+  pub_pressure.publish(&pressure_msg);
+  //pub_pressure.publish(&temperature_msg);
+
+  publisher_timer = millis() + 1000; //publish once a second
+
+  }
+
+  nh.spinOnce();
+}
