ae0f52e92f5cd001f24c47018c4ba01740c88ade
Anass Al <mr@anassinator.com>
2015-06-20 18:03:23 -0400

    Remove pinger frequency from Pinger

--- catkin_ws/src/hydrophones/src/hydrophones/models/pinger.py
+++ catkin_ws/src/hydrophones/src/hydrophones/models/pinger.py
@@ -9,14 +9,12 @@ class Pinger(object):
 
     """Pinger object."""
 
-    def __init__(self, (x, y, z), frequency):
+    def __init__(self, (x, y, z)):
         """Constructs Pinger instance.
 
         Args:
             (x, y, z): Coordinates of pinger relative to origin mic in meters.
-            frequency: Frequency of ping in Hz.
         """
         self.x = x
         self.y = y
         self.z = z
-        self.freq = frequency
