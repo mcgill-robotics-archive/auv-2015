475a91467465c0329f0d82789269affede054084
Anass Al <mr@anassinator.com>
2015-06-20 22:50:18 -0400

    Add random noise to multilateration in order to decrease the odds of a singular matrix

--- catkin_ws/src/hydrophones/src/hydrophones/solver/multilateration.py
+++ catkin_ws/src/hydrophones/src/hydrophones/solver/multilateration.py
@@ -15,6 +15,7 @@ http://en.wikipedia.org/wiki/Multilateration
 """
 
 import numpy as np
+from random import random
 from hydrophones.exceptions import CouldNotBeSolvedError
 
 __author__ = "Anass Al-Wohoush"
@@ -37,37 +38,51 @@ def solve(mics, tdoa, speed):
     """
     NUMBER_OF_MICS = len(mics)
 
-    # Construct vectors.
-    # A[m] = (2 * x[m]) / (v * t[m]) - (2 * x[1]) / (v * t[1])
-    # B[m] = (2 * y[m]) / (v * t[m]) - (2 * y[1]) / (v * t[1])
-    # C[m] = v * (t[m] - t[1]) - (x[m]^2 + y[m]^2) / (v * t[m])
-    #      + (x[1]^2 + y[1]^2) / (v * t[1])
-    A = [
-        (2 / speed) * ((mics[i].x / tdoa[i]) - (mics[1].x / tdoa[1]))
-        for i in range(2, NUMBER_OF_MICS)
+    # Force raise Error instead of printing RuntimeWarning.
+    np.seterr(all="raise")
+
+    # Add picosecond random noise to lower decrease the chances of having a
+    # singular matrix.
+    tdoa = [
+        dt + random() * 1e-12
+        for dt in tdoa
     ]
 
-    B = [
-        (2 / speed) * ((mics[i].y / tdoa[i]) - (mics[1].y / tdoa[1]))
-        for i in range(2, NUMBER_OF_MICS)
-    ]
-
-    C = [
-        speed * (tdoa[i] - tdoa[1]) + (
-            (mics[1].x ** 2 + mics[1].y ** 2) / tdoa[1] -
-            (mics[i].x ** 2 + mics[i].y ** 2) / tdoa[i]
-        ) / speed
-        for i in range(2, NUMBER_OF_MICS)
-    ]
-
-    # Solve by QR.
     try:
-        (x, y) = -np.linalg.solve(np.transpose([A, B]), C)
-    except np.linalg.LinAlgError as e:
-        # Matrix is either singular or impossible to solve.
-        raise CouldNotBeSolvedError(e)
+        # Construct vectors.
+        # All vectors were multiplied by t[1] * t[0] in order to avoid division
+        # by zero.
+        # A[m] = (2 * x[m]) / (v * t[m]) - (2 * x[1]) / (v * t[1])
+        # B[m] = (2 * y[m]) / (v * t[m]) - (2 * y[1]) / (v * t[1])
+        # C[m] = v * (t[m] - t[1]) - (x[m]^2 + y[m]^2) / (v * t[m])
+        #      + (x[1]^2 + y[1]^2) / (v * t[1])
+        A = [
+            (2 / speed) * ((mics[i].x * tdoa[1]) - (mics[1].x * tdoa[i]))
+            for i in range(2, NUMBER_OF_MICS)
+        ]
 
-    # TODO(anassinator): Verify for overflow.
-    pass
+        B = [
+            (2 / speed) * ((mics[i].y * tdoa[1]) - (mics[1].y * tdoa[i]))
+            for i in range(2, NUMBER_OF_MICS)
+        ]
+
+        C = [
+            tdoa[i] * tdoa[1] * speed * (tdoa[i] - tdoa[1]) -
+            (mics[i].x ** 2 + mics[i].y ** 2) * tdoa[1] / speed +
+            (mics[1].x ** 2 + mics[1].y ** 2) * tdoa[i] / speed
+            for i in range(2, NUMBER_OF_MICS)
+        ]
+
+        # Solve for (x, y) by QR.
+        # 0 = A[m]x + B[m]y + C[m]
+        print A, B, C
+        try:
+            (x, y) = -np.linalg.solve(np.transpose([A, B]), C)
+        except np.linalg.LinAlgError as e:
+            # Matrix is either singular or impossible to solve.
+            raise CouldNotBeSolvedError(e)
+    except (FloatingPointError, ZeroDivisionError) as e:
+        # Overflow, underflow, division by zero or invalid error.
+        raise CouldNotBeSolvedError(e)
 
     return (x, y)
