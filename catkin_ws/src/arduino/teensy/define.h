4698b599e68595f806cae86d543794485d1e8775
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2015-07-01 18:54:44 -0400

    change numbering of the pneumatics

--- catkin_ws/src/arduino/teensy/define.h
+++ catkin_ws/src/arduino/teensy/define.h
@@ -26,12 +26,12 @@
 #define PWM_FREQUENCY 93750 //One of the Ideal Frequency of teensy3.1
 
 //SOLENOID, one used pin for (Pin_33)
-#define SOLENOID_PIN_PORT_DROPPER           12
-#define SOLENOID_PIN_STARBOARD_DROPPER      24
-#define SOLENOID_PIN_PORT_GRABBER           25
-#define SOLENOID_PIN_STARBOARD_GRABBER      26
-#define SOLENOID_PIN_PORT_TORPEDO           27
-#define SOLENOID_PIN_STARBOARD_TORPEDO      28
+#define SOLENOID_PIN_STARBOARD_DROPPER      12
+#define SOLENOID_PIN_PORT_DROPPER           24
+#define SOLENOID_PIN_STARBOARD_GRABBER      25
+#define SOLENOID_PIN_PORT_GRABBER           26
+#define SOLENOID_PIN_STARBOARD_TORPEDO      27
+#define SOLENOID_PIN_PORT_TORPEDO           28
 #define SOLENOID_PIN_EXTRA                  33
 
 //ANALOG
