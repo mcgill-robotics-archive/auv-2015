6418d6fe9cd6403fc41e7f80d4ec5dad0963b157
Anass Al <mr@anassinator.com>
2015-07-15 10:18:21 -0400

    Clean up comm script

--- catkin_ws/src/nucleo/scripts/comm.py
+++ catkin_ws/src/nucleo/scripts/comm.py
@@ -3,14 +3,20 @@
 
 """Read from Nucleo serial."""
 
+from __future__ import print_function
+
 import time
 import bitstring
 from serial import Serial
 
 __author__ = "Anass Al-Wohoush"
 
-
+SIXTEEN_BITS = True
+LOOPS = 1000.0
 BUFFERSIZE = 6000
+
+raw_buffersize = 2 * BUFFERSIZE if SIXTEEN_BITS else BUFFERSIZE
+data_size = 16 if SIXTEEN_BITS else 8
 
 headers = (
     "[DATA 0]",
@@ -37,6 +43,7 @@ if __name__ == "__main__":
 
         header = "["
 
+        print("Waiting for data...")
         while ser.readable():
             skipped = 0
             while True:
@@ -53,21 +60,35 @@ if __name__ == "__main__":
                 header = ""
 
             # Process data.
-            print(header)
+            print(header, end=" ")
             if "DATA" in header:
-                raw = ser.read(2 * BUFFERSIZE)
+                # Stop timer.
                 end = time.time()
+
+                # Get data and determine ADC.
+                raw = ser.read(raw_buffersize)
                 _, i = header.strip("]").split()
                 instance = int(i)
-                if starts[instance]:
-                    dt = (end - starts[instance]) / 1000.0 / BUFFERSIZE
-                    print("dt: {}".format(dt))
-                starts[instance] = end
+
+                # Convert to array.
                 stream = bitstring.BitStream(bytes=raw)
-                data = list(stream.read(16).uintle for i in range(BUFFERSIZE))
-                print(sum(data) / len(data))
+                data = list(
+                    stream.read(data_size).uintle
+                    for i in range(BUFFERSIZE))
+
+                # Determine sampling frequency.
+                if starts[instance]:
+                    fs = 1 / ((end - starts[instance]) / LOOPS / BUFFERSIZE)
+                    print("fs: {}".format(fs), end=" ")
+
+                # Set timer.
+                starts[instance] = end
+
+                # Print mean.
+                mean = sum(data) / float(len(data))
+                print("mean: {}".format(mean))
             else:
-                print(ser.readline())
+                print(ser.readline().strip())
 
             # Reset header.
             header = ""
