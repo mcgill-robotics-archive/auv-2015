5620ff344d4a5bd44de39c0191bec479a10c5785
Anass Al <mr@anassinator.com>
2015-07-15 04:09:33 -0400

    Add write_string and logging capabilities to UART

--- catkin_ws/src/nucleo/src/uart.c
+++ catkin_ws/src/nucleo/src/uart.c
@@ -1,15 +1,12 @@
 #include "uart.h"
 
-UART_HandleTypeDef uart;
-
-
 /**
- * Initialize USART2.
+ * Initializes USART2.
  */
 void UART_Init(void)
 {
   uart.Instance = USART2;
-  uart.Init.BaudRate = 115200;
+  uart.Init.BaudRate = 460800;
   uart.Init.WordLength = UART_WORDLENGTH_8B;
   uart.Init.StopBits = UART_STOPBITS_1;
   uart.Init.Parity = UART_PARITY_NONE;
@@ -21,18 +18,50 @@ void UART_Init(void)
   HAL_UART_Init(&uart);
 }
 
+
 /**
- * Write buffer to UART.
+ * Writes buffer to UART.
  */
 void write_buffer(uint8_t* data, uint16_t size) {
   HAL_UART_Transmit(&uart, data, size, 1000);
 }
 
+
 /**
- * Write integer to UART.
+ * Writes string to UART.
+ */
+void write_string(char* data) {
+  HAL_UART_Transmit(&uart, data, strlen(data), 1000);
+}
+
+
+/**
+ * Writes integer to UART.
  */
 void write_int(int data) {
   char buff[10];
-  itoa(data, buff, 10);
-  HAL_UART_Transmit(&uart, buff, strlen(buff), 1000);
+  sprintf(buff, "%d", data);
+  HAL_UART_Transmit(&uart, buff, sizeof(buff), 1000);
+}
+
+
+/**
+ * Writes debug log to UART.
+ */
+void log_debug(char* data) {
+  #ifdef DEBUG
+  HAL_UART_Transmit(&uart, "[DEBUG] ", 8, 1000);
+  HAL_UART_Transmit(&uart, data, strlen(data), 1000);
+  HAL_UART_Transmit(&uart, "\r\n", 2, 1000);
+  #endif
+}
+
+
+/**
+ * Writes fatal log to UART.
+ */
+void log_fatal(char* data) {
+  HAL_UART_Transmit(&uart, "[FATAL] ", 8, 1000);
+  HAL_UART_Transmit(&uart, data, strlen(data), 1000);
+  HAL_UART_Transmit(&uart, "\r\n", 2, 1000);
 }
