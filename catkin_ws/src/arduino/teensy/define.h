79ae623933879e1a8845164ceb0d60b4987d74c0
Anass Al <mr@anassinator.com>
2015-07-19 21:06:08 -0700

    Remove battery voltage mapping in favor of raw readings

--- catkin_ws/src/arduino/teensy/define.h
+++ catkin_ws/src/arduino/teensy/define.h
@@ -64,15 +64,15 @@
 #define MOTOR_VOLTAGE_PIN                   A7
 #define MOTOR_CURRENT_PIN                   A6
 
-//
-const double kCOM_VOLT_SLOPE = 0.113390131; //(teensy voltage * total resistance / (single resisitance * mamotorCommandValue bit))
-const double kCOM_VOLT_OFFSET = -48.180;
+//POWER MONITORING
+const double kCOM_VOLT_SLOPE = 1.0;
+const double kCOM_VOLT_OFFSET = 0.0;
 
 const double kCOM_CURR_SLOPE = 1.0;
 const double kCOM_CURR_OFFSET = 0.0;
 
-const double kMOT_VOLT_SLOPE = 0.051530;
-const double kMOT_VOLT_OFFSET = -20.015;
+const double kMOT_VOLT_SLOPE = 1.0;
+const double kMOT_VOLT_OFFSET = 0.0;
 
 const double kMOT_CURR_SLOPE = 1.0;
 const double kMOT_CURR_OFFSET = 0.0;
