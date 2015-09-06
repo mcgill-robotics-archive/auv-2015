7a99289991a121952cce65fc70765eeff2a9c9a4
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2015-09-05 21:26:12 -0400

    update blinky

--- catkin_ws/src/arduino/blinky/blinky.ino
+++ catkin_ws/src/arduino/blinky/blinky.ino
@@ -2,13 +2,14 @@
 #include "sin_table.h"
 
 #define NUM_LED_PER_STRIP   25
-#define NUM_LED_STRIP       6
+#define NUM_LED_STRIP       7
 #define LED_COUNT           NUM_LED_PER_STRIP * NUM_LED_STRIP
 #define LED_OUT_PIN         13
-#define LED_BRIGHTNESS      120
+#define LED_BRIGHTNESS      40
+#define BAR_SIZE            3
 
-#define TIMEOUT_PERIOD      1000
-#define PHASE_STEP          30
+#define TIMEOUT_PERIOD      2000
+#define PHASE_STEP          50
 #define FRACTION_BIT_MASK   0x7FF
 #define NUM_FRACTION_BIT    11
 
@@ -19,7 +20,7 @@ long lastTime;
 
 void setup()
 {
-  Serial.begin(57600);
+  Serial.begin(115200);
   LEDS.addLeds<WS2812B, LED_OUT_PIN, GRB>(leds, LED_COUNT);
   LEDS.setBrightness(LED_BRIGHTNESS);
   LEDS.show();
@@ -79,42 +80,63 @@ void colorLoop() {
     setLedColor(i, pixelIndex, CRGB(255, 0, 0));
   }
 }
+  
+void drawBar(uint8_t location, bool isOnLeft){
+  if(location > (NUM_LED_STRIP - BAR_SIZE)){
+    return;
+  }  
+  uint8_t x = 1;
+  if(isOnLeft) {
+    x = 23;
+  }
+  for (uint8_t i = 0; i < BAR_SIZE; i++) {
+    setLedColor(x,location + i, CRGB(255, 0, 0));
+  }
+}
+
+void drawPong(uint8_t x, uint8_t y){
+  if(y > 12 ){
+    y = 12;
+  }
+  if(x > 6){
+    return;
+  }
+  setLedColor(2 * y, x, CRGB(255, 255, 255));
+}
 
 void serialLoop() {
-  static int pixelIndex;
 
   unsigned long lastReceiveTime = millis();
 
+  static uint8_t buff;
   while (true) {
-
-    if (Serial.available() > 2) {
-      lastReceiveTime = millis();
-
-      uint8_t buffer[3]; // Buffer to store three incoming bytes used to compile a single LED color
-
-      for (uint8_t x = 0; x < 3; x++) { // Read three incoming bytes
-        uint8_t c = Serial.read();
-
-        if (c < 255) {
-          buffer[x] = c; // Using 255 as a latch semaphore
-        } else {
-          LEDS.show();
-          pixelIndex = 0;
-          break;
-        }
-
-        if (x == 2) {   // If we received three serial bytes
-          if (pixelIndex == LED_COUNT) {
-            break; // Prevent overflow by ignoring the pixel data beyond LED_COUNT
-          }
-          leds[pixelIndex] = CRGB(buffer[0], buffer[1], buffer[2]);
-          pixelIndex++;
-        }
+    if (buff != 255) {
+      if (Serial.available()) {
+        buff = Serial.read();
       }
     }
-
+    
+    if(buff == 255){
+       if(Serial.available() > 3){
+          for (uint16_t i = 0; i < LED_COUNT; i++) {
+            leds[i] = CRGB::Black;
+          }
+          
+          drawBar(Serial.read(),false);
+          drawBar(Serial.read(),true);
+          drawPong(Serial.read(),Serial.read());
+          buff = 0;
+          lastReceiveTime = millis();
+          LEDS.show();
+       }
+    }
+    
+    
     // If we haven't received data in 1 second, return to playing back our animation
     if (millis() > lastReceiveTime + TIMEOUT_PERIOD) {
+      while(Serial.available()){
+        Serial.read();
+      }
       return;
     }
   }
@@ -123,7 +145,7 @@ void serialLoop() {
 void loop()
 {
   // If we get some data, switch to passthrough mode
-  if (Serial.available() > 0) {
+  if (Serial.available()) {
     serialLoop();
   }
 
